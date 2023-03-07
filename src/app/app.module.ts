584c18fb7b7416e47fbc30d9fea7cf5653e1d79c
Ekaterina Romanenko <kat.romanenko24@gmail.com>
2023-03-07 19:23:20 +0600

    Черновик

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { WhyComponent } from './why/why.component';
+import { AchievementComponent } from './achievement/achievement.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { CallComponent } from './call/call.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    WhyComponent,
+    AchievementComponent,
+    TestimonialComponent,
+    CallComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
